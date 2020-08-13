a59412d0910a62131b3b069c7df9791c2ce354e4
manu <manu.villar.vitoria@gmail.com>
2020-08-13 11:51:39 +0200

    adding React Router DOM & Components

---
+++ src/components/EditarProducto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditarProducto = () => {
+    return (
+        <h1>desde EditarProducto.js</h1>
+    )
+}
+
+export default EditarProducto
---
+++ src/components/Productos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productos = () => {
+    return (
+      <h1>desde Productos.js</h1>
+    )
+}
+
+export default Productos
