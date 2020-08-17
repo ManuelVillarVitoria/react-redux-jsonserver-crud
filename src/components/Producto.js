934aece74eb36a009e9219507ca7346d4e0e5296
manu <manu.villar.vitoria@gmail.com>
2020-08-17 21:42:53 +0200

    taking the user to Edit

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 import { useDispatch } from 'react-redux';
@@ -10,6 +10,7 @@ const Producto = ({producto}) => {
     const { nombre, precio, id } = producto;
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const confirmarEliminarProducto = id => {
 
@@ -30,14 +31,22 @@ const Producto = ({producto}) => {
     }
 
 
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
+
+    }
+
     return (
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold"> $ {precio} </span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">
+                <button 
+                    type="button"
+                    onClick={ () => redireccionarEdicion(producto) }
+                    className="btn btn-primary mr-2">
                     Editar
-                </Link>
+                </button>
                 <button
                     type="button"
                     className="btn btn-danger"
